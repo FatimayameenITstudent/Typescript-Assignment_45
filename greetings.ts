7900cb474b5ac51920bd0506fc211752e49aeb1f
FatimayameenITstudent <162579764+FatimayameenITstudent@users.noreply.github.com>
2024-04-15 15:03:37 +0500

    Add files via upload

---
+++ greetings.ts
@@ -0,0 +1,10 @@
+
+let names :string[] = ['Yahya' , 'usman' ,'wali' , 'Sana', 'yameen'] ;
+
+const message:string = "Welcome to typescript class: "
+
+for(let i=0; i<names.length; i++) {
+
+console.log(message + names[i]) ;
+
+}
