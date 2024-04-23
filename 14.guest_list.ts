dd760299fa1041444c4cc0a0cd0ebef7460d80dc
FatimayameenITstudent <162579764+FatimayameenITstudent@users.noreply.github.com>
2024-04-23 20:33:57 +0500

    Add files via upload

---
+++ 14.guest_list.ts
@@ -0,0 +1,6 @@
+let guest_list: string []=['amir' , 'anwar' , 'kashaan'];
+
+for(let i=0; i<guest_list.length; i++ ){
+
+    console.log('Dear Mr. ' + guest_list[i] + ', \n\nIt is our pleasure to invite you in our party. \n\nThank you!')
+}
